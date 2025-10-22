e8f4994f520087ee44dfa46d6e01953ac9b94a83
Jie-Yan094 <jarita0904@gmail.com>
2025-10-22 02:30:51 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 st.title("Streamlit 核心 Widgets")
 # 1.把Widgets放到側邊攔(sidebar)
 with st.sidebar:
-st.header("這裡是側邊攔")
+    st.header("這裡是側邊攔")
 # 選擇框(Selectbox)
 option = st.selectbox(
 "你最喜歡的GIS軟體?",
@@ -16,11 +16,11 @@ st.write(f"你選的軟體是: {option}")
 st.write(f"你選的年份是: {year}")
 #按鈕(Button)
 if st.button("點我顯示氣球!"):
-st.balloons()
+    st.balloons()
 # 檔案上傳 (File Uploader) - 地理系必備!
 uploaded_file = st.file_uploader(
 "上傳你的Shapefile (.zip) 或 GeoTIFF (.tif) 或 GeoJSON (.json)",
 type=["zip", "tif", "json"]
 )
 if uploaded_file is not None:
-st.success(f"你上傳了: {uploaded_file.name} (大小: {uploaded_file.size} bytes)")
+    st.success(f"你上傳了: {uploaded_file.name} (大小: {uploaded_file.size} bytes)")
